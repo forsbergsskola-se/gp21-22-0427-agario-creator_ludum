67e77762f71c495b710740f0df4b7598ed7357e8
Oliver Källerfors <idetorka@hotmail.com>
2022-04-27 22:33:32 +0200

    Added most of OpenWord_MMO (Not working)

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Program{
+    
+    public static async Task Main(){
+        string message = "";
+        var udpClient = new UdpClient("127.0.0.1",10002);
+        var remoteEP = new IPEndPoint(IPAddress.Any, 10003); 
+
+        while (true){
+            Console.WriteLine("Waiting for connection...");
+            var udpReceivedData =  udpClient.Receive(ref remoteEP);
+            Console.WriteLine("Connection accepted.");
+            var uudpReceivedDataAsString = udpReceivedData.ToString();
+            
+            if (udpReceivedData == null){
+                throw new NullReferenceException();
+            }
+
+            if (!IsWord(uudpReceivedDataAsString)){
+                throw new InvalidDataException();
+            }
+
+            message +=  uudpReceivedDataAsString + " ";
+
+            Console.WriteLine("Sending Data...");
+            await udpClient.SendAsync(Encoding.ASCII.GetBytes(message),remoteEP);
+            Console.WriteLine("Data Sent.");
+            Console.WriteLine("Closing Client...");
+            udpClient.Close();
+            Console.WriteLine("Client Closed.");
+        }
+        
+        
+    }
+    
+    static bool IsWord(String text) {
+        return (text.Length > 0 && text.Length <= 20 && !(text.Split(",.;: ".ToCharArray()).Length > 1));
+    }
+}
