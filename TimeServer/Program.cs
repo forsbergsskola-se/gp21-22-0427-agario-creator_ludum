19ec448465a446d0f6185b7a942edc6c22544769
Oliver Källerfors <idetorka@hotmail.com>
2022-04-27 15:34:00 +0200

    Implemented TimeServer

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Program{
+
+    
+    
+    public static async Task Main(){
+
+        var endpoint = new IPEndPoint(
+            // IP-Address: Used with IP-Protocol to find the right computer
+            IPAddress.Loopback, // 127.0.0.1 
+            // Port: Used with TCP / UDP Protocol to find the right program on a computer
+            12345
+        );
+        
+        TcpListener tcpListener = new TcpListener(endpoint);
+        
+        while (true){
+            tcpListener.Start();
+            Console.WriteLine("Awaiting for connection");
+            var tcpClient = await tcpListener.AcceptTcpClientAsync();
+            Console.WriteLine("Connection accepted");
+            
+            string currentDateTime = DateTime.Now.ToString();
+            string message = $"Connection Successful! Current time is : {currentDateTime}";
+            var encodedMessage = Encoding.ASCII.GetBytes(message);
+            
+            
+            tcpClient.GetStream().Write(encodedMessage);
+            tcpClient.GetStream().Close();
+            tcpClient.Close();
+        }
+    }
+    
+    
+}
