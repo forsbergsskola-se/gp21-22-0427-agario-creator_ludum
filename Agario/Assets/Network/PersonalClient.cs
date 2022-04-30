11f4eee77699f947ff04d81bcf7120e8b22a4017
Oliver Källerfors <idetorka@hotmail.com>
2022-04-30 15:10:44 +0200

    Fixed client not closing on aplicationQuit

--- Agario/Assets/Network/PersonalClient.cs
+++ Agario/Assets/Network/PersonalClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -7,11 +8,20 @@ using UnityEngine;
 public class PersonalClient : MonoBehaviour{
    static readonly IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Loopback, 9000);
    static readonly IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Loopback, 9001);
-   TcpClient tcpClient = new TcpClient(clientEndpoint);
+   TcpClient tcpClient;
 
 
 
    public void Connect(){
-       tcpClient.Connect(serverEndpoint);
+       tcpClient = new TcpClient(clientEndpoint);
+       tcpClient.BeginConnect(serverEndpoint.Address,serverEndpoint.Port,BeginConnectCallback,tcpClient);
+   }
+
+   void BeginConnectCallback(IAsyncResult callbackResult){
+       tcpClient.EndConnect(callbackResult);
+   }
+   
+   void OnApplicationQuit(){
+       tcpClient.Close(); 
    }
 }
