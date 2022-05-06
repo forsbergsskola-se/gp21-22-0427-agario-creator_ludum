3f8dc1d056f1b7992c0c193478ff29ba04784539
Oliver Källerfors <idetorka@hotmail.com>
2022-05-06 22:08:52 +0200

    Added and tweaked Events

--- Agario/Assets/_Scripts/Events/PlayerUnityEventSO.cs
+++ Agario/Assets/_Scripts/Events/PlayerUnityEventSO.cs
@@ -7,11 +7,11 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName = "New Player Unity Event", menuName = "Events/Unity Events/Player Event")]
 public class PlayerUnityEventSO : ScriptableObject
 {
-    public UnityEvent<Player> playerUnityEventSo;
-
-    void OnEnable(){
-        if (playerUnityEventSo == null){
-            playerUnityEventSo = new UnityEvent<Player>();
-        }
-    }
+    // public UnityEvent<Player> playerUnityEventSo;
+    //
+    // void Awake(){
+    //     if (playerUnityEventSo == null){
+    //         playerUnityEventSo = new UnityEvent<Player>();
+    //     }
+    // }
 }
--- Agario/Assets/_Scripts/Events/UnityEventSO.cs
+++ Agario/Assets/_Scripts/Events/UnityEventSO.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+
 [CreateAssetMenu(fileName = "New Unity Event", menuName = "Events/Unity Events/Unity Event")]
 public class UnityEventSO : ScriptableObject{
-    public UnityEvent<String> unityEventSo;
+    public UnityEvent unityEventSo;
+
+    void Awake(){
+        DontDestroyOnLoad(this);
+    }
 
     void OnEnable(){
         if (unityEventSo == null){
-            unityEventSo = new UnityEvent<String>();
+            unityEventSo = new UnityEvent();
         }
     }
 }
